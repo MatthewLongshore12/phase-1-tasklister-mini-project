8d37ae4eccebdcdb2bf2dcb8666ad3fdb845990a
Matthew Longshore <matthewlongshore12@gmail.com>
2023-02-01 11:54:50 -0500

    list working still need delete function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.getElementById('create-task-form');
+const uList = document.getElementById('tasks');
+
+function addTaskToList(eventObj) {
+  eventObj.preventDefault();
+  const listItem = document.createElement('li');
+  const newText = eventObj.target['new-task-description'].value;
+  listItem.textContent = newText;
+  uList.append(listItem);
+}
+
+form.addEventListener('submit', addTaskToList);
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  
+})
+
