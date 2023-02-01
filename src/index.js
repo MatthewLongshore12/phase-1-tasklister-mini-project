5fbb80a2b7564914aae549d74a07d6d120c5e5d6
Matthew Longshore <matthewlongshore12@gmail.com>
2023-02-01 12:57:53 -0500

    trying to figure out a delete button

--- src/index.js
+++ src/index.js
@@ -7,11 +7,23 @@ function addTaskToList(eventObj) {
   const newText = eventObj.target['new-task-description'].value;
   listItem.textContent = newText;
   uList.append(listItem);
+  createButton(listItem);
 }
 
 form.addEventListener('submit', addTaskToList);
 
+function createButton() {
+  const btn = document.createElement('button');
+  btn.textContent = "X";
+  uList.append(btn);
+  btn.addEventListener('click', createButton);
+}
 
+function deleteTask() {
+  
+}
+
+removeEventListener('click',);
 
 
 document.addEventListener('DOMContentLoaded', () => {
