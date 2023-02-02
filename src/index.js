d678420280047f8c8c50ed3ca685094727f69b0c
Matthew Longshore <matthewlongshore12@gmail.com>
2023-02-01 22:31:44 -0500

    finally added a working delete button

--- src/index.js
+++ src/index.js
@@ -7,23 +7,19 @@ function addTaskToList(eventObj) {
   const newText = eventObj.target['new-task-description'].value;
   listItem.textContent = newText;
   uList.append(listItem);
-  createButton(listItem);
+
+  const btn = document.createElement('button');
+  btn.textContent = "X";
+  listItem.append(btn);
+
+  btn.addEventListener('click', () => {
+    listItem.remove();
+  })
 }
 
 form.addEventListener('submit', addTaskToList);
 
-function createButton() {
-  const btn = document.createElement('button');
-  btn.textContent = "X";
-  uList.append(btn);
-  btn.addEventListener('click', createButton);
-}
 
-function deleteTask() {
-  
-}
-
-removeEventListener('click',);
 
 
 document.addEventListener('DOMContentLoaded', () => {
